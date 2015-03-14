048bfc70c5f9ab831e39706784e21ad529ee808d
Carolina Powers <carolinapoloni@gmail.com>
2015-03-13 23:41:02 -0400

    trying to create owners users on firebase

--- src/app/scripts/controllers/ownerbutton.js
+++ src/app/scripts/controllers/ownerbutton.js
@@ -9,7 +9,7 @@
 angular.module('whatsPup')
     .controller('OwnerBtnCtrl', function ($firebaseArray, $firebaseObject, Auth, $state) {
         var ref = new Firebase("https://whatspup.firebaseio.com/");
-
+        var newownerUser = {};
         //        this.ownerlogin = Auth.sitterlogin;
         this.ownerlogin = function () {
 
@@ -26,6 +26,37 @@ angular.module('whatsPup')
             })
         };
 
+        function updateUser(refdUser) {
+            console.log(refdUser)
+            if (refdUser === null) {
+                return null;
+            }
+
+            var ownerUser = ref.child('petowner').child(refdUser.facebook.id);
+            console.log(ownerUser);
+
+            ownerUser.update({
+                uid: refdUserdUser.facebook.id,
+                facebook: refdUser.facebook,
+                fullName: refdUser.facebook.displayName,
+                firstName: refdUser.facebook.cachedUserProfile.first_name,
+                lastName: refdUser.facebook.cachedUserProfile.last_name,
+                avatarUrl: refdUser.facebook.cachedUserProfile.picture.data.url,
+                gender: refdUser.facebook.cachedUserProfile.gender
+            });
+
+            //    // Set user to the object reference of authdUser
+            this.ownerUser = $firebaseObject(ref
+                    .child('petowner')
+                    .child(refdUser.facebook.id)
+                )
+                //            //
+                //            //    //stores the user information for use elsewhere
+            newownerUser = ownerUser;
+
+            return ownerUser;
+        };
+
 
         Auth.onAuth(function (user) {
             self.user = user;
@@ -35,5 +66,4 @@ angular.module('whatsPup')
                 console.log(user)
             }
         });
-
     });
