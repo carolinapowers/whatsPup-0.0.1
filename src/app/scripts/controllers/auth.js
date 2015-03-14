88277e3c476a9a5a68ce395e04afae88be593b5a
Carolina Powers <carolinapoloni@gmail.com>
2015-03-13 21:09:17 -0400

    added owner button to a different html and added it to main.html through ng-include

--- src/app/scripts/controllers/auth.js
+++ src/app/scripts/controllers/auth.js
@@ -4,7 +4,7 @@ angular.module('whatsPup')
     .controller('AuthCtrl', function ($firebaseArray, $firebaseObject, Auth) {
         var ref = new Firebase("https://whatspup.firebaseio.com/");
         this.sitterlogin = Auth.sitterlogin;
-        //        this.loggedIn = Auth.loggedIn;
+
         Auth.onAuth(function (user) {
             self.user = user;
             if (user === null) {
@@ -15,29 +15,3 @@ angular.module('whatsPup')
         });
 
     });
-
-//'use strict';
-//
-//angular.module('whatsPup')
-//    .controller('AuthCtrl', function ($firebaseArray, $firebaseObject, $location, $state) {
-//        var ref = new Firebase("https://whatspup.firebaseio.com/client");
-//
-//        this.sitterlogin = function () {
-//            ref.authWithOAuthPopup("facebook", function (error, authData) {
-//                if (error) {
-//                    console.log("Login Failed!", error);
-//                } else {
-//
-//                    console.log("Authenticated successfully with payload:", authData);
-//                    $state.go('client');
-//                }
-//                //                remember: "sessionOnly"
-//            })
-//        };
-//
-//        this.logout = function () {
-//            ref.unauth();
-//        };
-//
-//
-//    });
