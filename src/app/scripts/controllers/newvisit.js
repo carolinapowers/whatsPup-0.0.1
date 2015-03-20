3579d79428e49f59af3080fc832339a4314a3eb3
Jared Stevick <jstevick@gmail.com>
2015-03-20 14:10:39 -0400

    we can upload images and send them in the email

--- src/app/scripts/controllers/newvisit.js
+++ src/app/scripts/controllers/newvisit.js
@@ -18,113 +18,114 @@ angular.module('whatsPup')
 
         console.log(this.sendEmail);
 
+        this.image = '';
+
+
+        document.getElementById("upload_widget_opener").addEventListener("click", function() {
+            cloudinary.openUploadWidget({ 
+                cloud_name: 'jaredstevick',
+                upload_preset: 'xnpszqid'
+            }, 
+            function(error, result) { 
+                console.log(result[0].url)
+                self.image = result[0].url;
+            });
+            return self.image 
+        }, false);
+
+        
 
 
 
-
-
-        //        clientVisit.on("value", function (snapshot) {
-        //            self.getEmail = snapshot.val();
-        //            console.log("Client E-mail: " + self.getEmail.email);
-        //            return self.getEmail = self.getEmail.email;
-        //        });
-        //
-        //        console.log(self.getEmail);
-        // var food = this.food;
-        // var water = this.water;
-        // var play = this.play;
-        // var treats = this.treats;
-        // var meds = this.meds;
-        // var mess = this.mess;
-        // var packages = this.packages;
-        // var mail = this.mail;
-        // var plants = this.plants;
-        // var other = this.other;
-        // var message = this.message;
         var currentdate = new Date();
         var time = (currentdate.getMonth() + 1) + "/" + currentdate.getDate() + "/" + currentdate.getFullYear() + " at " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds(); //get time
+        
         this.sentEmail = function () {
             $http({
-                    method: "POST",
-                    url: "https://mandrillapp.com/api/1.0/messages/send-template.json",
-                    data: {
-                        'key': 'SjfF7oGr1BHLUnBlnSF20A',
-                        "template_name": "whatspup1",
-                        "template_content": [
-                            {
-                                "name": "example name",
-                                "content": "example content"
-                            }
-                        ],
-                        'message': {
-                            'from_email': 'whatspupupdate@gmail.com',
-                            'from_name': 'WhatsPup',
-                            'headers': {
-                                'Reply-To': 'whatspupupdate@gmail.com'
-                            },
-                            
-                            'subject': 'New Visit Update from WhatsPup',
-                            
-                            'to': [
-                                {
-                                    'email': this.getEmail,
-                                    'name': 'name',
-                                    'type': 'to'
-                            }],
+                method: "POST",
+                url: "https://mandrillapp.com/api/1.0/messages/send-template.json",
+                data: {
+                    'key': 'SjfF7oGr1BHLUnBlnSF20A',
+                    "template_name": "whatspup1",
+                    "template_content": [
+                        {
+                            "name": "example name",
+                            "content": "example content"
+                        }
+                    ],
+                    'message': {
+                        'from_email': 'whatspupupdate@gmail.com',
+                        'from_name': 'WhatsPup',
+                        'headers': {
+                            'Reply-To': 'whatspupupdate@gmail.com'
+                        },
                         
-                        "global_merge_vars": [
+                        'subject': 'New Visit Update from WhatsPup',
+                        
+                        'to': [
                             {
-                                "name": "time",
-                               "content": time
-                            },
-                            {
-                                "name": "food",
-                                "content": this.food
-                            },
-                            {
-                                "name": "water",
-                                "content": this.water
-                            },
-                            {
-                                "name": "play",
-                                "content": this.play
-                            },
-                            {
-                                "name": "treats",
-                                "content": this.treats
-                            },
-                            {
-                                "name": "meds",
-                                "content": this.meds
-                            },
-                            {
-                                "name": "mess",
-                                "content": this.mess
-                            },
-                            {
-                                "name": "packages",
-                                "content": this.packages
-                            },
-                            {
-                                "name": "mail",
-                                "content": this.mail
-                            },
-                            {
-                                "name": "plants",
-                                "content": this.plants
-                            },
-                            {
-                                "name": "other",
-                                "content": this.other
-                            },
-                            {
-                                "name": "message",
-                                "content": this.message
-                            }
-                        ]
+                                'email': this.getEmail,
+                                'name': 'name',
+                                'type': 'to'
+                        }],
+                    
+                    "global_merge_vars": [
+                        {
+                            "name": "time",
+                           "content": time
+                        },
+                        {
+                            "name": "food",
+                            "content": this.food
+                        },
+                        {
+                            "name": "water",
+                            "content": this.water
+                        },
+                        {
+                            "name": "play",
+                            "content": this.play
+                        },
+                        {
+                            "name": "treats",
+                            "content": this.treats
+                        },
+                        {
+                            "name": "meds",
+                            "content": this.meds
+                        },
+                        {
+                            "name": "mess",
+                            "content": this.mess
+                        },
+                        {
+                            "name": "packages",
+                            "content": this.packages
+                        },
+                        {
+                            "name": "mail",
+                            "content": this.mail
+                        },
+                        {
+                            "name": "plants",
+                            "content": this.plants
+                        },
+                        {
+                            "name": "other",
+                            "content": this.other
+                        },
+                        {
+                            "name": "message",
+                            "content": this.message
+                        },
+                        {
+                            "name": "image",
+                            "content": this.image
+                        }
+                    ]
                     }
-                    }
-                })
+                }
+            })
                 .success(function (response) {
                     alert('The visit has been saved. Thank you!'); // show success message
                     console.log(response);
